1b84df905992b51ce5892adf46731670a7abca36
Tutorial Doctor <TutorialDoctor@users.noreply.github.com>
2016-01-05 17:16:17 -0500

    Update animated_view.py

--- Projects/UI/animated_view.py
+++ Projects/UI/animated_view.py
@@ -1,30 +1,32 @@
 # Jan 2, 2016, 10:12 AM
 # Tutorial Doctor
 
-# Add a ui to this file and add a custom view named view2. Add your controls to view2 and run the script. 
-# view 2 should animate in when you press the +, and animate out when you press the greater than sign.
+# Add a ui to this file and add a custom view named view2. Add your
+# controls to view2 and run the script.  view 2 should animate in
+# when you press the +, and animate out when you press the greater than sign.
 # Additionaly, some toolbar options should be added to the main view.
 
 import ui,sound
 print ui
+
 # Action Functions
-#--------------------------------------------------------------------	
+#--------------------------------------------------------------------
 def move_left(sender):
 	def left():
-		view2.x=0
+		view2.x = 0
 		#view2 is a custom view located to the right of the main view
 		# add suviews to this view if you please.
-		r[0].action=move_right
-	r[0].title='>'
-	ui.animate(left,.5)
+		r[0].action = move_right
+	r[0].title = '>'
+	ui.animate(left, .5)
 
 
 def move_right(sender):
 	def right():
-		r[0].action=move_left
+		r[0].action = move_left
 		view2.x = window.width
-	r[0].title='+'
-	ui.animate(right,.5)
+	r[0].title = '+'
+	ui.animate(right, .5)
 
 
 def close(sender):
@@ -35,69 +37,60 @@ def error(sender):
 	sound.play_effect('Error')
 def clip(sender):
 	sound.play_effect('Clock_1')
-#--------------------------------------------------------------------	
+#--------------------------------------------------------------------
 
 
 # Setup
-#--------------------------------------------------------------------	
+#--------------------------------------------------------------------
 window = ui.load_view()
-view2=window['view2']
+view2 = window['view2']
 view2.x = window.width
 view2.height = window.height
 view2.width = window.width
-#--------------------------------------------------------------------	
+#--------------------------------------------------------------------
 
 
 # Functions
-#--------------------------------------------------------------------	
-def create_l_buttonItems(*buttons):
-	items=[]
-	for b in buttons:
-		b=ui.ButtonItem(b)
-		b.tint_color='white'
-		items.append(b)
-	return items
+#--------------------------------------------------------------------
+def create_l_buttonItems(titles):
+	def make_button_item(title):
+		button = ui.ButtonItem(b)
+		button.enabled = title != '|'
+		button.tint_color = 'white'
+		return button
+	return [make_button_item(title) for title in titles]
 
-
-def create_r_buttonItems(*buttons):
-	items=[]
-	for b in buttons:
-		b=ui.ButtonItem(b)
-		b.tint_color='white'
-		b.action=move_left
-		if b.title=='>':
-			b.action=move_right
-		items.append(b)
-	return items
-#--------------------------------------------------------------------	
+def create_r_buttonItems(titles):
+	def make_button_item(title):
+		button = ui.ButtonItem(b)
+		button.tint_color = 'white'
+		button.action = move_right if title == '>' else move_left
+		return button
+	return [make_button_item(title) for title in titles]
+#--------------------------------------------------------------------
 
 
 # Implementation
-#--------------------------------------------------------------------	
-l = create_l_buttonItems('File','|','Edit','|','About')
+#--------------------------------------------------------------------
+l = create_l_buttonItems('File | Edit | About'.split())
 window.left_button_items = l
 r = create_r_buttonItems('+')
 window.right_button_items = r
-l[1].enabled=False 
-l[3].enabled=False 
-window.present('fullscreen',title_bar_color='#635D51')
-
-
-#--------------------------------------------------------------------	
+window.present('fullscreen', title_bar_color='#635D51')
+#--------------------------------------------------------------------
 
 
 # Testing
-#--------------------------------------------------------------------	
-for item in l:
-	print item.title
-for item in r:
-	print item.title
-#--------------------------------------------------------------------	
+#--------------------------------------------------------------------
+for menu in (l, r):
+	print(', '.join(item.title for item in l))
+#--------------------------------------------------------------------
 
 
 # Notes and discoveries
-#--------------------------------------------------------------------	
-# If you write good functions, the implementation should be short and simple to type and understand. Comments shouldnt be needed in this case.
+#--------------------------------------------------------------------
+# The readability of code can be improved by avoiding single character variable names.
+# If you write good functions, the implementation should be short and simple to understand.
+# Comments shouldn't be needed in this case.
 # This code is still not as refactorable as it can be.
-#--------------------------------------------------------------------	
-
+#--------------------------------------------------------------------
