8ed194bd6f84b6fa543c28e89fcf3e991b735460
TutorialDoctor <thetutorialdoctor@gmail.com>
2016-07-15 19:12:45 -0400

    Code update

--- Projects/Apps/Basic Bible/Basic Bible.py
+++ Projects/Apps/Basic Bible/Basic Bible.py
@@ -1,78 +1,59 @@
-# coding: utf-8
-import sqlite3,ui,sound
-
-# http://simoncozens.github.io/open-source-bible-data/
-# https://github.com/scrollmapper/bible_databases
-# #http://sebastianraschka.com/Articles/2014_sqlite_in_python_tutorial.html
-# v.action = lambda sender: myfunc("foo", sender, "bar", something=42)
-
-# f-strings? @ccc posted about this on the forums.
-
-database = ['kjv.db','bible-sqlite']
-con = sqlite3.connect(database[0])
-cursor = con.cursor()
+import ui,sqlite3
 
 
-class Query:
-	def get_chapter(sender):
-		sound.play_effect('ui:rollover2')
-		return chapters.data_source.items[sender.selected_row]
+def updates(*args):
+	con = sqlite3.connect('bible-sqlite.db')
+	cursor=con.cursor()
+	tbl_books = args[0]
+	tbl_chapters = args[1]
+	tbl_testaments = args[2]
+	
+	all_bks_query = 'select n from key_english'
+	all_bks = [x[0] for x in cursor.execute(all_bks_query)]
+	
+	ot_query = 'select n from key_english where b < 40'
+	ot_bks = [x[0] for x in cursor.execute(ot_query)]
+	
+	nt_query = 'select n from key_english where b > 40'
+	nt_bks = [x[0] for x in cursor.execute(nt_query)]
+	
+	tbl_books.items = all_bks
+	selected_book = tbl_books.items[tbl_books.selected_row]
+	selected_chap = tbl_chapters.items[tbl_chapters.selected_row]
+	selected_testament = tbl_testaments.items[tbl_testaments.selected_row]
 
-	def get_testament(sender):
-		sound.play_effect('ui:rollover2')
-		return testaments.data_source.items[sender.selected_row]
+	#if selected_testament=='Old Testament':
+		#tbl_books.items=ot_bks
+	
+	#elif selected_testament=='New Testament':
+		#tbl_books.items= nt_bks
 
-	def get_book(sender):
-		sound.play_effect('ui:rollover2')
-		return books.data_source.items[sender.selected_row]
+	num_query = "select b from key_english where n='{}'".format(selected_book)
+	bk_num=[x for x in cursor.execute(num_query)][0][0]
+	
+	c = selected_chap
+	
+	txt_query = "select c,v,t from 't_kjv' where b = '{}' AND c = '{}'".format(bk_num,c)
+	txt = [row for row in cursor.execute(txt_query)]
+	txt_formatted = "\n".join("{} {}: {}\n".format('*',c,t) for b,c,t in txt)
+	
+	if txt_formatted=='':
+		contents.text = 'Chapter does not exist'
+	
+	else: contents.text = txt_formatted
+	heading.text=selected_book+' '+str(selected_chap)
 
-def activate_table(tbl,src,act):
-	li = ui.ListDataSource(src)
-	tbl.data_source = li
-	tbl.data_source.action = act
-	tbl.delegate = li
 
-
-bible = ui.load_view('Basic Bible')
-contents=bible['contents']
-
-chapters = bible['chapters']
+# Implementation
+bible = ui.load_view()
+heading = bible['book_heading']
 books = bible['books']
+chapters = bible['chapters']
+contents = bible['contents']
 testaments = bible['testaments']
-
-
-chapter_src = [i for i in range(1,21)]
-activate_table(chapters,chapter_src,Query.get_chapter)
-
-testament_src = ['Old Testament','New Testament']
-activate_table(testaments,testament_src,Query.get_testament)
-
-book_src = ['Book','Book','Book','Romans']
-activate_table(books,book_src,Query.get_book)
-
-
-def scripture(book,chapter):
-	sql = "select book,chapter,content from 'bible' where book = '{}' AND chapter = '{}'".format(book,chapter)
-	l = [row for row in cursor.execute(sql)]
-	s = "\n".join("{} {}: {}\n".format(book,chapter,content) for book,chapter,content in l)
-	return s
-
-contents.text = scripture('Rom','2')
-
+#books.data_source = ui.ListDataSource(l)
+#books.data_source.items = l
+f = lambda sender: updates(sender,chapters.data_source,testaments.data_source)
+books.data_source.items = [x[0] for x in sqlite3.connect('bible-sqlite.db').execute('select n from key_english')]
+books.data_source.action = f
 bible.present(orientations=['landscape'])
-
-
-
-# ADDITIONAL HELPER CODE
-#cursor.execute("SELECT * FROM sqlite_master")
-
-
-def all_in_table():
-	for row in cursor.execute("SELECT * FROM bible"):
-		print(row)
-
-def all_tables():
-	for row in cursor.execute("SELECT name FROM sqlite_master WHERE type='table';"):
-		print(row)	
-
-
