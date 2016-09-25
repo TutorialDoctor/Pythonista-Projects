07e63d5a0d90e98c4b7bce76270896a92e677c55
Tutorial Doctor <TutorialDoctor@users.noreply.github.com>
2016-09-24 22:25:50 -0400

    Update dictionary.py

--- Projects/Apps/Dictionary/dictionary.py
+++ Projects/Apps/Dictionary/dictionary.py
@@ -11,6 +11,13 @@ class MyTextFieldDelegate (object):
 			try:
 				txt_definition.text=data[textfield.text.upper()]
 			except: None
+		"""
+		with open('graph2.json',encoding='utf-8') as infile2:
+			data2 = json.load(infile2)
+			try:
+				txt_graph.text=str(data2[textfield.text.upper()])
+			except: None
+		"""
 	def textfield_should_return(self, textfield):
 		#textfield.end_editing()
 		return True
@@ -23,8 +30,11 @@ class MyTextFieldDelegate (object):
 # A JSON representation of Webster's Unabridged Dictionary
 
 v=ui.load_view('dictionaryUI')
+v.name='Dictionary'
+v.tint_color='white'
+v.background_color=.21, .21, .21
 fld_word = v['fld_word']
 fld_word.delegate = MyTextFieldDelegate()
 txt_definition = v['txt_definition']
-v.present()
-
+txt_graph = v['txt_graph']
+v.present(title_bar_color='#c54242',title_color='white')
